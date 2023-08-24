609f20287166c7d35fbcb0afcda54d6df1d7e399
jadeallan <58634970+allanjade@users.noreply.github.com>
2023-08-24 15:59:51 +0300

    Add files via upload

--- blog/settings.py
+++ blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-+au4r9-$4od+ivz4ym_la)-ot9#k2pna)e5-i_63@gc@d^-fv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
--- blog/wsgi.py
+++ blog/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
 
 application = get_wsgi_application()
+
+# vercel configuration/ brigding connection to vercel app
+app = application
